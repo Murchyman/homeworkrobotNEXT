627fbb367fbd8c526224b8302870f750f05c8440
John Doe <johndoe@example.com>
2022-02-07 17:44:38 +1000

    fixed cross origin mis spelling

--- components/Layout.js
+++ components/Layout.js
@@ -10,7 +10,7 @@ const Layout = ({ children }) => {
                 <title>HomeWork Robot</title>
                 <meta name="description" content="With HomeWork Robot, get instant answers to questions or writing prompts" />
                 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6052671390695750"
-                    crossorigin="anonymous"></script>
+                    crossOrigin="anonymous"></script>
             </Head>
             <div className={styles.Main}>
                 <Navbar />
