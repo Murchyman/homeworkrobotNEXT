29ef2fcda37c842af341ab8918c3249ecf82fc15
Murchyman <N11099887@qut.edu.au>
2022-02-10 18:11:16 +1000

    small mix

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Button from '@mui/material/Button';
-import Image from 'next/image'
-import example from '../assets/example.png'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link';
 import DeadReportForm from '../components/DeadReportForm';
@@ -23,8 +21,8 @@ export default function Home() {
           <span className={styles.paragraph}>Homework Robot is an Ai driven study assistant that lets you focus on the important stuff while our artificial intelligence delivers you high quality notes and reports. Best of all Homework Robot is 100% free to use!
           </span>
           <div className={styles.buttonContainer}>
-            <Link href='/Generator'><Button className="navbar-brand" style={{ minWidth: '16em', minHeight: '4em' }} variant="contained">Get Started</Button></Link>
-            <Link href='/About'><Button className="navbar-brand" style={{ minWidth: '16em', minHeight: '4em' }} variant="outlined">Learn More</Button></Link>
+            <Link href='/Generator'><Button className="navbar-brand" style={{ minWidth: '17em', minHeight: '4em' }} variant="contained">Get Started</Button></Link>
+            <Link href='/About'><Button className="navbar-brand" style={{ minWidth: '17em', minHeight: '4em' }} variant="outlined">Learn More</Button></Link>
 
           </div>
 
