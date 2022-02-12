8a97300e05ad9305bcc58b5bc4c6732adc68a988
John Doe <johndoe@example.com>
2022-02-12 14:55:57 +1000

    final about

--- pages/About.js
+++ pages/About.js
@@ -20,19 +20,31 @@ const About = () => {
                 </div>
 
                 <div className={styles.imageArea}>
-                    <Image src='/icons/email.svg' width={1000} height={700} />
+                    <Image src='/icons/WritingRobot.svg' width={1000} height={700} />
 
                 </div>
 
                 <div className={styles.HeaderBox}>
                     <div className={styles.heading}>
-                        <span> <span className={styles.orange}>Homework Robot</span> was created from a dream to make <span className={styles.blue}>Ai assisted studying</span> accessible to <span className={styles.pink}>as many students as possible</span></span>
+                        <span>Who's Involved?</span>
                     </div>
                     <div className={styles.subHeading}>
-                        <span>Made with 💖 in <span className={styles.orange}>Queensland</span>.</span>
+                        <span> We are a growing team based in Queensland Australia. We hire professionals from all across the globe to create our unique products and services.</span>
                     </div>
                     <div className={styles.subHeading}>
-                        <span>Homework Robot is an artificial intelligence based completion algorithm leveraging the powerful <span className={styles.blue}>GPT-3</span> machine learning technology.</span>
+                        <span>Some key employees include</span>
+                        <ul>
+                            <li>
+                                <span>Mitchell Benjamin (CEO)</span>
+                            </li>
+                            <li>
+                                <span>John Stevens (CTO)</span>
+                            </li>
+                            <li>
+                                <span>Janice Crane (Secretary)</span>
+                            </li>
+
+                        </ul>
                     </div>
 
                 </div>
@@ -42,7 +54,7 @@ const About = () => {
             {/* 
 
                 
-                Neo  is a growing team based in Queensland Australia. We hire professionals from all across the globe to create our unique products and services.
+               
 
                 Our team is dedicated to providing the best AI assisted education tools and resources to our users.
 
