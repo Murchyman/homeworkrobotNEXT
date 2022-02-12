0b0d9b225e84975c5874a77f8ffdd9b87a7d6d5c
John Doe <johndoe@example.com>
2022-02-12 14:38:18 +1000

    about dev

--- pages/About.js
+++ pages/About.js
@@ -1,22 +1,59 @@
 import React from 'react'
-
+import styles from '../styles/About.module.css'
+import Image from 'next/image'
 const About = () => {
     return (
         <>
+            <div className={styles.container}>
 
-            <span>Homework Robot is an artificial intelligence based completion algorithm using the powerful GPT-3 machine learning technology.
+                <div className={styles.HeaderBox}>
+                    <div className={styles.heading}>
+                        <span> <span className={styles.orange}>Homework Robot</span> was created from a dream to make <span className={styles.blue}>Ai assisted studying</span> accessible to <span className={styles.pink}>as many students as possible</span></span>
+                    </div>
+                    <div className={styles.subHeading}>
+                        <span>Made with 💖 in <span className={styles.orange}>Queensland</span>.</span>
+                    </div>
+                    <div className={styles.subHeading}>
+                        <span>Homework Robot is an artificial intelligence based completion algorithm leveraging the powerful <span className={styles.blue}>GPT-3</span> machine learning technology.</span>
+                    </div>
 
-                Made with 💖 in Queensland by Neo.
+                </div>
 
-                Homework Robot was created from a dream to make Ai assisted studying accessible to as many students as possible
+                <div className={styles.imageArea}>
+                    <Image src='/icons/email.svg' width={1000} height={700} />
+
+                </div>
+
+                <div className={styles.HeaderBox}>
+                    <div className={styles.heading}>
+                        <span> <span className={styles.orange}>Homework Robot</span> was created from a dream to make <span className={styles.blue}>Ai assisted studying</span> accessible to <span className={styles.pink}>as many students as possible</span></span>
+                    </div>
+                    <div className={styles.subHeading}>
+                        <span>Made with 💖 in <span className={styles.orange}>Queensland</span>.</span>
+                    </div>
+                    <div className={styles.subHeading}>
+                        <span>Homework Robot is an artificial intelligence based completion algorithm leveraging the powerful <span className={styles.blue}>GPT-3</span> machine learning technology.</span>
+                    </div>
+
+                </div>
+
+            </div>
+
+            {/* 
+
+                
                 Neo  is a growing team based in Queensland Australia. We hire professionals from all across the globe to create our unique products and services.
 
                 Our team is dedicated to providing the best AI assisted education tools and resources to our users.
 
                 If you have any questions or feedback, please don't hesitate to get in touch!
 
-                Email: contact@neo.education
-            </span>
+                We are always looking to add new features and improve the product.
+
+                If you are interested in working with us, please get in touch! */}
+
+
+
 
 
         </>
